f3e864d5bfaf60a3493ab98d5d64a30d6cd17dfe
talesm <tales.miranda84@gmail.com>
2023-06-26 21:19:11 -0300

    Add imguir

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <SDL.h>
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer.h"
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
@@ -11,21 +14,50 @@ main()
   window = SDL_CreateWindow("Pixedit viewer",
                             SDL_WINDOWPOS_UNDEFINED,
                             SDL_WINDOWPOS_UNDEFINED,
-                            800,
-                            600,
+                            1024,
+                            768,
                             SDL_WINDOW_RESIZABLE);
   renderer = SDL_CreateRenderer(window, -1, 0);
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |=
+    ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  io.ConfigFlags |=
+    ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+  // Setup Dear ImGui style
+  // ImGui::StyleColorsDark();
+  // ImGui::StyleColorsLight();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer_Init(renderer);
 
   bool exited = false;
   while (!exited) {
     for (SDL_Event ev; SDL_PollEvent(&ev);) {
+      ImGui_ImplSDL2_ProcessEvent(&ev);
       switch (ev.type) {
       case SDL_QUIT: exited = true; break;
       default: break;
       }
     }
+
+    // Update
+    ImGui_ImplSDLRenderer_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow();
+
+    /// Render
     SDL_SetRenderDrawColor(renderer, 60, 60, 60, 255);
     SDL_RenderClear(renderer);
+
+    ImGui::Render();
+    ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
 
     SDL_RenderPresent(renderer);
     SDL_Delay(0);
