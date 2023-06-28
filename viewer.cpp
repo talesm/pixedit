644b6eed6b1a3d01ce70b194c9bce052dd547efb
talesm <tales.miranda84@gmail.com>
2023-06-28 17:59:21 -0300

    Don't focus on key initially

--- viewer.cpp
+++ viewer.cpp
@@ -122,8 +122,8 @@ ViewerApp::setupImGui()
   ImGui::CreateContext();
   ImGuiIO& io = ImGui::GetIO();
   (void)io;
-  io.ConfigFlags |=
-    ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  // io.ConfigFlags |=
+  //   ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
   io.ConfigFlags |=
     ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
 
