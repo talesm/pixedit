9d2386ef5e174cd279e5994e99c0181bea141f6c
talesm <tales.miranda84@gmail.com>
2023-07-01 22:42:54 -0300

    Bresenham algorithm

---
+++ src/algorithms/rasterLine.hpp
@@ -0,0 +1,74 @@
+#ifndef PIXEDIT_SRC_ALGORITHMS_LINE_INCLUDED
+#define PIXEDIT_SRC_ALGORITHMS_LINE_INCLUDED
+
+#include <concepts>
+#include <cstdlib>
+
+namespace pixedit {
+
+/// @brief Implement half open line alorithm
+/// @tparam CALLBACK any callable that accepts two coordinates x, y;
+/// @param xBeg initial x
+/// @param yBeg initial y
+/// @param xEnd final x
+/// @param yEnd final
+/// @param callback the callback
+///
+/// Line algorithm: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+template<std::invocable<int, int> CALLBACK>
+void
+rasterLineOpen(int xBeg, int yBeg, int xEnd, int yEnd, CALLBACK callback)
+{
+  auto rasterOctant = [](int deltaX, int deltaY, auto callback) {
+    if (deltaY == 0) {
+      for (int x = 0; x < deltaX; ++x) callback(x, 0);
+      return;
+    }
+    int error = 0;
+    int y = 0;
+    for (int x = 0; x < deltaX; ++x) {
+      callback(x, y);
+      error += deltaY;
+      if (error > deltaX / 2) {
+        y++;
+        error -= deltaX;
+      }
+    }
+  };
+  auto rasterQuadrant = [&](int deltaX, int deltaY, auto callback) {
+    if (deltaX >= deltaY) {
+      rasterOctant(deltaX, deltaY, callback);
+    } else {
+      rasterOctant(deltaY, deltaX, [&](int y, int x) { callback(x, y); });
+    }
+  };
+
+  int deltaX = xEnd - xBeg, deltaY = yEnd - yBeg;
+  if (deltaY >= 0) {
+    if (deltaX >= 0)
+      rasterQuadrant(
+        deltaX, deltaY, [&](int x, int y) { callback(xBeg + x, yBeg + y); });
+    else
+      rasterQuadrant(
+        -deltaX, deltaY, [&](int x, int y) { callback(xBeg - x, yBeg + y); });
+  } else {
+    if (deltaX >= 0)
+      rasterQuadrant(
+        deltaX, -deltaY, [&](int x, int y) { callback(xBeg + x, yBeg - y); });
+    else
+      rasterQuadrant(
+        -deltaX, -deltaY, [&](int x, int y) { callback(xBeg - x, yBeg - y); });
+  }
+}
+
+template<std::invocable<int, int> CALLBACK>
+void
+rasterLine(int xBeg, int yBeg, int xEnd, int yEnd, CALLBACK callback)
+{
+  rasterLineOpen(xBeg, yBeg, xEnd, yEnd, callback);
+  callback(xEnd, yEnd);
+}
+
+} // namespace pixedit
+
+#endif /* PIXEDIT_SRC_ALGORITHMS_LINE_INCLUDED */
---
+++ test/algorithms/rasterLineTest.cpp
@@ -0,0 +1,76 @@
+#include "rasterLine.hpp"
+#include <tuple>
+#include <vector>
+#include "../catch.hpp"
+
+using namespace pixedit;
+
+using Point = std::tuple<int, int>;
+using PointVector = std::vector<Point>;
+
+TEST_CASE("Test rasterLine", "[raster][line]")
+{
+  PointVector points;
+  auto callback = [&points](int x, int y) { points.emplace_back(x, y); };
+  SECTION("Begin == End")
+  {
+    rasterLineOpen(10, 10, 10, 10, callback);
+    REQUIRE(points.empty());
+  }
+  SECTION("End is 3px to the right")
+  {
+    rasterLineOpen(10, 10, 13, 10, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{11, 10});
+    REQUIRE(points.at(2) == Point{12, 10});
+  }
+  SECTION("End is 3px to the left")
+  {
+    rasterLineOpen(10, 10, 7, 10, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{9, 10});
+    REQUIRE(points.at(2) == Point{8, 10});
+  }
+  SECTION("End is 3px down")
+  {
+    rasterLineOpen(10, 10, 10, 13, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{10, 11});
+    REQUIRE(points.at(2) == Point{10, 12});
+  }
+  SECTION("End is 3px up")
+  {
+    rasterLineOpen(10, 10, 10, 7, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{10, 9});
+    REQUIRE(points.at(2) == Point{10, 8});
+  }
+  SECTION("End is 3px down right")
+  {
+    rasterLineOpen(10, 10, 13, 13, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{11, 11});
+    REQUIRE(points.at(2) == Point{12, 12});
+  }
+  SECTION("End is 3px down left")
+  {
+    rasterLineOpen(10, 10, 7, 13, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{9, 11});
+    REQUIRE(points.at(2) == Point{8, 12});
+  }
+  SECTION("End is 3px up right")
+  {
+    rasterLineOpen(10, 10, 13, 7, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{11, 9});
+    REQUIRE(points.at(2) == Point{12, 8});
+  }
+  SECTION("End is 3px up left")
+  {
+    rasterLineOpen(10, 10, 7, 7, callback);
+    REQUIRE(points.at(0) == Point{10, 10});
+    REQUIRE(points.at(1) == Point{9, 9});
+    REQUIRE(points.at(2) == Point{8, 8});
+  }
+}
