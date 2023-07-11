fe28ca0fcd406fa75c2e4f1d9f13051bc0147b56
talesm <tales.miranda84@gmail.com>
2023-07-10 21:42:41 -0300

    Move method definition out of class

--- src/EditorApp.cpp
+++ src/EditorApp.cpp
@@ -76,81 +76,7 @@ private:
     for (auto& buffer : buffers) { showPictureWindow(buffer); }
   }
 
-  void showPictureWindow(const std::shared_ptr<PictureBuffer>& buffer)
-  {
-    auto& settings = viewSettings[buffer.get()];
-    // TODO Measure title and decoration instead of guessing
-    ImGui::SetNextWindowSize(ImVec2(buffer->getW() + 16, buffer->getH() + 35),
-                             ImGuiCond_Once);
-    if (ImGui::Begin(buffer->getFilename().c_str())) {
-      bool redraw = false;
-      ImVec2 canvasSz = ImGui::GetContentRegionAvail();
-      if (canvasSz.x < 50.0f) canvasSz.x = 50.0f;
-      if (canvasSz.y < 50.0f) canvasSz.y = 50.0f;
-      {
-        int picW, picH;
-        SDL_QueryTexture(settings.texture, nullptr, nullptr, &picW, &picH);
-        if (settings.texture == nullptr || picW != canvasSz.x ||
-            picH != canvasSz.y) {
-          SDL_DestroyTexture(settings.texture);
-          settings.texture = SDL_CreateTexture(renderer,
-                                               SDL_PIXELFORMAT_ABGR32,
-                                               SDL_TEXTUREACCESS_TARGET,
-                                               canvasSz.x,
-                                               canvasSz.y);
-          SDL_SetTextureBlendMode(settings.texture, SDL_BLENDMODE_BLEND);
-          redraw = true;
-        }
-      }
-      ImVec2 canvasP0 = ImGui::GetCursorScreenPos();
-      ImVec2 canvasP1 =
-        ImVec2(canvasP0.x + canvasSz.x, canvasP0.y + canvasSz.y);
-
-      ImGuiIO& io = ImGui::GetIO();
-
-      if (ImGui::IsWindowFocused()) {
-        ImGui::InvisibleButton("Canvas",
-                               canvasSz,
-                               ImGuiButtonFlags_MouseButtonLeft |
-                                 ImGuiButtonFlags_MouseButtonRight |
-                                 ImGuiButtonFlags_MouseButtonMiddle);
-        const bool is_hovered = ImGui::IsItemHovered(); // Hovered
-        const bool is_active = ImGui::IsItemActive();   // Held
-        if (is_hovered || is_active) {
-          view.state.x = io.MousePos.x - canvasP0.x;
-          view.state.y = io.MousePos.y - canvasP0.y;
-          view.state.left = ImGui::IsMouseDown(ImGuiMouseButton_Left);
-          view.state.middle = ImGui::IsMouseDown(ImGuiMouseButton_Middle);
-          view.state.right = ImGui::IsMouseDown(ImGuiMouseButton_Right);
-          view.state.wheelX += io.MouseWheelH;
-          view.state.wheelY += io.MouseWheel;
-        }
-      }
-      if (ImGui::IsWindowFocused() || redraw) {
-        view.scale = settings.scale;
-        view.offset = settings.offset;
-        view.setBuffer(buffer);
-        SDL_SetRenderTarget(renderer, settings.texture);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-        SDL_RenderClear(renderer);
-        auto vp = view.getViewport();
-        view.setViewport({0, 0, int(canvasSz.x), int(canvasSz.y)});
-        view.update(renderer);
-        view.render(renderer);
-
-        if (ImGui::IsWindowFocused()) {
-          settings.scale = view.scale;
-          settings.offset = view.offset;
-        }
-
-        view.setViewport(vp);
-        SDL_SetRenderTarget(renderer, nullptr);
-      }
-      ImDrawList* draw_list = ImGui::GetWindowDrawList();
-      draw_list->AddImage(settings.texture, canvasP0, canvasP1);
-    }
-    ImGui::End();
-  }
+  void showPictureWindow(const std::shared_ptr<PictureBuffer>& buffer);
 
   void showMainMenuBar();
 
@@ -336,6 +262,82 @@ EditorApp::close()
       if (showView) { view.setBuffer(buffers[bufferIndex]); }
     }
   }
+}
+
+void
+EditorApp::showPictureWindow(const std::shared_ptr<PictureBuffer>& buffer)
+{
+  auto& settings = viewSettings[buffer.get()];
+  // TODO Measure title and decoration instead of guessing
+  ImGui::SetNextWindowSize(ImVec2(buffer->getW() + 16, buffer->getH() + 35),
+                           ImGuiCond_Once);
+  if (ImGui::Begin(buffer->getFilename().c_str())) {
+    bool redraw = false;
+    ImVec2 canvasSz = ImGui::GetContentRegionAvail();
+    if (canvasSz.x < 50.0f) canvasSz.x = 50.0f;
+    if (canvasSz.y < 50.0f) canvasSz.y = 50.0f;
+    {
+      int picW, picH;
+      SDL_QueryTexture(settings.texture, nullptr, nullptr, &picW, &picH);
+      if (settings.texture == nullptr || picW != canvasSz.x ||
+          picH != canvasSz.y) {
+        SDL_DestroyTexture(settings.texture);
+        settings.texture = SDL_CreateTexture(renderer,
+                                             SDL_PIXELFORMAT_ABGR32,
+                                             SDL_TEXTUREACCESS_TARGET,
+                                             canvasSz.x,
+                                             canvasSz.y);
+        SDL_SetTextureBlendMode(settings.texture, SDL_BLENDMODE_BLEND);
+        redraw = true;
+      }
+    }
+    ImVec2 canvasP0 = ImGui::GetCursorScreenPos();
+    ImVec2 canvasP1 = ImVec2(canvasP0.x + canvasSz.x, canvasP0.y + canvasSz.y);
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    if (ImGui::IsWindowFocused()) {
+      ImGui::InvisibleButton("Canvas",
+                             canvasSz,
+                             ImGuiButtonFlags_MouseButtonLeft |
+                               ImGuiButtonFlags_MouseButtonRight |
+                               ImGuiButtonFlags_MouseButtonMiddle);
+      const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+      const bool is_active = ImGui::IsItemActive();   // Held
+      if (is_hovered || is_active) {
+        view.state.x = io.MousePos.x - canvasP0.x;
+        view.state.y = io.MousePos.y - canvasP0.y;
+        view.state.left = ImGui::IsMouseDown(ImGuiMouseButton_Left);
+        view.state.middle = ImGui::IsMouseDown(ImGuiMouseButton_Middle);
+        view.state.right = ImGui::IsMouseDown(ImGuiMouseButton_Right);
+        view.state.wheelX += io.MouseWheelH;
+        view.state.wheelY += io.MouseWheel;
+      }
+    }
+    if (ImGui::IsWindowFocused() || redraw) {
+      view.scale = settings.scale;
+      view.offset = settings.offset;
+      view.setBuffer(buffer);
+      SDL_SetRenderTarget(renderer, settings.texture);
+      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+      SDL_RenderClear(renderer);
+      auto vp = view.getViewport();
+      view.setViewport({0, 0, int(canvasSz.x), int(canvasSz.y)});
+      view.update(renderer);
+      view.render(renderer);
+
+      if (ImGui::IsWindowFocused()) {
+        settings.scale = view.scale;
+        settings.offset = view.offset;
+      }
+
+      view.setViewport(vp);
+      SDL_SetRenderTarget(renderer, nullptr);
+    }
+    ImDrawList* draw_list = ImGui::GetWindowDrawList();
+    draw_list->AddImage(settings.texture, canvasP0, canvasP1);
+  }
+  ImGui::End();
 }
 
 void
