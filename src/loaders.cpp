02f275a9ae233d51f20e713cf0582030081b668b
talesm <tales.miranda84@gmail.com>
2023-07-20 19:52:53 -0300

    Support to pix on loaders

--- src/loaders.cpp
+++ src/loaders.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <SDL_image.h>
 #include "PictureBuffer.hpp"
+#include "PixReader.hpp"
 #include "Surface.hpp"
 #include "utils/replayPicture.hpp"
 
@@ -24,9 +25,51 @@ loadBuffer(const std::string& filename, Id loader)
 }
 
 static Surface
+makePixSurface(const PictureFormat& format, std::span<Uint8> data)
+{
+  SDL_PixelFormatEnum pixelFormat;
+  switch (format.bitsPerPixel) {
+  case 32: pixelFormat = SDL_PIXELFORMAT_ABGR32; break;
+  case 24: pixelFormat = SDL_PIXELFORMAT_BGR24; break;
+  case 16: pixelFormat = SDL_PIXELFORMAT_BGR565; break;
+  case 8: pixelFormat = SDL_PIXELFORMAT_INDEX8; break;
+  default: return nullptr;
+  }
+  SDL_Surface* s = SDL_CreateRGBSurfaceWithFormat(
+    0, format.w, format.h, format.bitsPerPixel, pixelFormat);
+  if (!s) { return nullptr; }
+  if (SDL_ISPIXELFORMAT_INDEXED(pixelFormat)) {
+    SDL_Color grayscale[256];
+    for (int i = 0; i < 256; ++i) {
+      grayscale[i] = {Uint8(i), Uint8(i), Uint8(i), 255};
+    }
+
+    SDL_SetPaletteColors(s->format->palette, grayscale, 0, 256);
+  }
+  auto bytesPerPixel = s->format->BytesPerPixel;
+  Uint8* pixels = static_cast<Uint8*>(s->pixels);
+  auto it = data.begin();
+  for (int y = 0; y < format.h; ++y) {
+    auto end = it + bytesPerPixel * format.w;
+    std::copy(it, end, pixels);
+    it = end;
+    pixels += s->pitch;
+  }
+  return {s, true};
+}
+
+static Surface
 doLoadSurface(const std::string& filename, Id loader)
 {
-  if (loader == loaders::PIX) { return nullptr; }
+  if (loader == loaders::PIX) {
+    Surface s;
+    SDL_RWops* rw = SDL_RWFromFile(filename.c_str(), "rb");
+    readPixImage(rw, [&s](const PictureFormat& format, std::span<Uint8> data) {
+      s = makePixSurface(format, data);
+    });
+    SDL_RWclose(rw);
+    return s;
+  }
   if (loader == loaders::SDL2_IMAGE) {
     return {IMG_Load(filename.c_str()), true};
   }
